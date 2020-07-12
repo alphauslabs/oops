c4efc4f98404a59a568b8df22175b4a491eb28aa
flowerinthenight <flowerinthenight@pm.me>
2020-07-12 23:15:33 +0900

    fix args propagation

--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module github.com/flowerinthenight/oops
 go 1.14
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/flowerinthenight/longsub v0.0.5
 	github.com/gavv/httpexpect/v2 v2.1.0
-	github.com/pkg/errors v0.8.0
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/klauspost/compress v1.10.10 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/onsi/ginkgo v1.14.0 // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/satori/go.uuid v1.2.0
+	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/pflag v1.0.5
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/valyala/fasthttp v1.14.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0
-	k8s.io/klog/v2 v2.3.0
 )
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/flowerinthenight/longsub"
 	"github.com/spf13/cobra"
 )
 
@@ -21,8 +22,15 @@ var (
 		RunE:  runE,
 	}
 
+	region  string
+	key     string
+	secret  string
+	rolearn string
+
 	files   []string
 	dir     string
+	snssqs  string
+	slack   string
 	verbose bool
 )
 
@@ -58,10 +66,39 @@ func runE(cmd *cobra.Command, args []string) error {
 	})
 }
 
+func processSQS(ctx interface{}, data []byte) error {
+	log.Printf("%v", string(data))
+	return nil
+}
+
 func run(ctx context.Context, done chan error) {
+	lsu := longsub.NewAWSUtil(region, key, secret, rolearn)
+	_, err := lsu.SetupSnsSqsSubscription(snssqs, snssqs)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("%v subscribed to %v", snssqs, snssqs)
+
+	ctx0, _ := context.WithCancel(ctx)
+	done0 := make(chan error, 1)
+	go func() {
+		ls := longsub.NewSqsLongSub(nil, snssqs, processSQS,
+			longsub.WithRegion(region),
+			longsub.WithAccessKeyId(key),
+			longsub.WithSecretAccessKey(secret),
+			longsub.WithRoleArn(rolearn),
+		)
+
+		err := ls.Start(ctx0, done0)
+		if err != nil {
+			log.Printf("start long processing for %v failed, err=%v", snssqs, err)
+		}
+	}()
+
 	<-ctx.Done()
 	log.Print("oops stopped")
-	done <- nil
+	done <- <-done0
 }
 
 func runCmd() *cobra.Command {
@@ -90,12 +127,18 @@ func runCmd() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().StringVar(&snssqs, "sns-sqs", snssqs, "name of the SNS topic, same name will be used in SQS")
 	return cmd
 }
 
 func init() {
+	rootcmd.PersistentFlags().StringVar(&region, "region", os.Getenv("AWS_REGION"), "AWS region")
+	rootcmd.PersistentFlags().StringVar(&key, "aws-key", os.Getenv("AWS_ACCESS_KEY_ID"), "AWS access key")
+	rootcmd.PersistentFlags().StringVar(&secret, "aws-secret", os.Getenv("AWS_SECRET_ACCESS_KEY"), "AWS secret key")
+	rootcmd.PersistentFlags().StringVar(&rolearn, "aws-rolearn", os.Getenv("ROLE_ARN"), "AWS role ARN to assume")
 	rootcmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", verbose, "verbose mode")
 	rootcmd.PersistentFlags().StringVarP(&dir, "dir", "d", dir, "root directory for scenario file[s]")
+	rootcmd.PersistentFlags().StringVar(&slack, "slack-url", slack, "slack url for notification")
 	rootcmd.Flags().StringSliceVarP(&files, "scenarios", "s", files, "scenario file[s] to run, comma-separated, or multiple -s")
 	rootcmd.AddCommand(runCmd())
 }
